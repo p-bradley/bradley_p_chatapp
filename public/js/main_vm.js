0fef608dc41948cbacb624a99420561bf2cd573f
p-bradley <p_bradley@fanshaweonline.ca>
2020-02-04 15:09:30 -0500

    need 2 fix

--- public/js/main_vm.js
+++ public/js/main_vm.js
@@ -1,17 +1,60 @@
 // imports always go first - if we're importing anything
+import ChatMessage from "./modules/ChatMessage.js";
 
 const socket = io();
 
-function setUserId(packet) {
-    debugger;
-    console.log(packet);
+function setUserId({sID, message}) {
+    //debugger;
+    vm.socketID = sID;
+    //vm = viewmodel
 }
-
-//some event handling -> these events are coming from the server
-socket.addEventListener('connected', setUserId);
-socket.addEventListener('user_disconnect', runDisconnectMessage);
 
 function runDisconnectMessage(packet) {
     debugger;
     console.log(packet);
 }
+
+function appendNewMessage(msg) {
+    vm.messages.push(msg);
+}
+
+//this is our main Vue instance
+const vm = new Vue({
+    data: {
+        socketID: "",
+        messages: [],
+        message: "",
+        nickName: ""
+ },
+
+    methods: {
+        dispatchMessage() {
+            // emit a message event and send the message to the server
+            console.log('handle send message');
+
+            socket.emit('chat_message', { 
+                content: this.message,
+                name: this.nickName || "anonymous"
+                // || is called a double pipe operator or an "or" operator
+                // if this.nickName is set, use it as the value
+                // or just make name "anonymous"
+            })
+
+            this.message = "";
+        }
+    },
+
+    components: {
+        newmessage: ChatMessage
+    },
+
+    mounted: function() {
+        console.log('mounted');
+    }
+
+}).$mount("#app")
+
+//some event handling -> these events are coming from the server
+socket.addEventListener('connected', setUserId);
+socket.addEventListener('user_disconnect', runDisconnectMessage);
+socket.addEventListener('new_message', appendNewMessage);
